2475a7791fc2e204275b1095565a7ee0078327b0
MVDLNE <madelinelora95@icloud.com>
2025-03-08 12:31:56 -0500

    done adding error handling

--- main.py
+++ main.py
@@ -23,32 +23,50 @@ except requests.exceptions.RequestException as e:
     
 
 # [PHISHSTATS]
-phish_response = requests.get('https://phishstats.info:2096/api/phishing?_where=')
-phish_response_json = json.loads(phish_response.text)
+try:    
+    phish_response = requests.get('https://phishstats.info:2096/api/phishing?_where=')
+    phish_response.raise_for_status()
 
-for item in phish_response_json:
-    try:
-        print(item['url'])
-    except Exception as e:
-        print('An error occurred: {e}')
+    try: 
+        phish_response_json = phish_response.json()
+    except json.JSONDecodeError as e:
+        print(f'Failed to decode JSON: {e}')
+    else:
+        for item in phish_response_json:
+            try:
+                print(item['url'])
+            except KeyError as e:
+                print(f'Missing expected key: {e}')
+except requests.exceptions.RequestException as e:
+    print(f'Error fetching data: {e}')
 
-# [Phishing.Database]
+# [Phishing.Database] 
 repository_url = 'https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master/phishing-links-NEW-today.txt'
-page = requests.get(repository_url)
-soup = BeautifulSoup(page.text, 'html.parser')
 
-try: 
-    print(soup.contents)
-except Exception as e:
-    print('An error occurred: {e}')
+try:
+    page = requests.get(repository_url, timeout=10)
+    page.raise_for_status()
+
+    try:
+        soup = BeautifulSoup(page.text, 'html.parser')
+        print(soup.contents)
+    except Exception as e:
+        print(f'An error occurred while parsing HTML: {e}')
+except requests.exceptions.RequestException as e:
+    print(f'Error fetching data: {e}')
 
 # [OPENPHISH]
 openphish_url = 'https://www.openphish.com/feed.txt'
-openphish_page = requests.get(openphish_url)
-phish_soup = BeautifulSoup(openphish_page.text, 'html.parser')
 
-try:    
+try: 
+    openphish_page = requests.get(openphish_url, timeout=10)
+    page.raise_for_status()
+
+    try:
+        phish_soup = BeautifulSoup(openphish_page.text, 'html.parser')   
     # Print the content
-    print(phish_soup.contents)
-except Exception as e:
-    print('An error occurred: {e}')
+        print(phish_soup.contents)
+    except Exception as e:
+        print(f'An error occurred while parsing HTML: {e}')
+except requests.exceptions.RequestException as e:
+    print(f'Error fetching data: {e}')
